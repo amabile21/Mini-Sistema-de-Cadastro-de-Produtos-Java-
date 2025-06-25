cd408e5c207483559802000d20af219879684729
amabile21 <amabiletalia@gmail.com>
2025-06-25 19:55:30 -0300

    Mini Sistema de Cadastro de Produtos

---
+++ README.md
@@ -0,0 +1,19 @@
+# 🛒 Mini Sistema de Cadastro de Produtos (Java)
+
+O sistema permite que o usuário cadastre um produto, visualize as informações e opte por cadastrar novos produtos.
+
+## 🧠 Conceitos Aplicados
+
+- **Encapsulamento** com `private` e uso de `get`/`set`
+- **Construtores** para inicializar objetos
+- **Laço de repetição (`while`)**
+- **Uso do Scanner** para entrada de dados via terminal
+
+---
+
+## 📦 Estrutura do Projeto
+
+- `Produto.java`: classe que representa um produto, com atributos `nome`, `preço` e `código`. Inclui métodos para cadastrar e exibir informações.
+- `Main.java`: classe principal que executa o programa e interage com o usuário.
+
+---
---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("--- CADASTRO DE PRODUTO ---");
+        System.out.print("Nome do produto: ");
+        String nome = input.nextLine();
+
+        System.out.print("Preço: ");
+        double preco = input.nextDouble();
+
+        System.out.print("Código: ");
+        int codigo = input.nextInt();
+
+        Produto produto1 = new Produto(nome, preco, codigo);
+
+        System.out.println("Deseja cadastrar novo produto?");
+        System.out.println("1 - SIM");
+        System.out.println("2 - NÃO");
+        String opcao = input.next();
+
+        while (opcao.equals("1")) {
+            produto1.cadastrarNovoProduto();
+        }
+
+        System.out.println("Produto cadastrado!");
+        produto1.exibirInfo();
+
+
+        input.close();
+    }
+}
